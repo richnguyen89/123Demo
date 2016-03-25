799f76b4627a903ce221aa4d7c013e1a26943346
Rich Nguyen <giau.nguyen@smartdev.vn>
2016-03-25 09:36:55 +0700

    Initial Commit

---
+++ BookManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BookManager
+//
+//  Created by Rich Nguyen on 3/25/16.
+//  Copyright © 2016 Rich Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
